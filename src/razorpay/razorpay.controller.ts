f2ba10225062a75419f236720dfd333b70b473ae
SyamSkyLab <syamtechy5858@gmail.com>
2025-06-04 15:57:43 +0530

    bug fix

--- src/razorpay/razorpay.controller.ts
+++ src/razorpay/razorpay.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Headers, Post, Req, Res } from '@nestjs/common';
 import { RazorpayService } from './razorpay.service';
 import { Request, Response } from 'express';
-
+import { Public } from 'src/login/public.decorator';
+@Public()
 @Controller('razorpay')
 export class RazorpayController {
   constructor(private readonly razorpayService: RazorpayService) {
